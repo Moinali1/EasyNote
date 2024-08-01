c857af722eaaa7a96f38cf0e1a8941df2171e3a6
Moin Ali <moinali7863@gmail.com>
2024-08-01 15:39:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
+  var [details, setDetails] = useState([]);
+
+  function addNote(heading, content) {
+    setDetails((preValue) => [
+      ...preValue,
+      {
+        headingDetail: heading,
+        contentDetail: content
+      }
+    ]);
+    console.log(details);
+  }
+
+  function deleteNode(Nodekey) {
+    console.log("delete run");
+    setDetails((preValue) => {
+      return preValue.filter((detail, index) => index !== Nodekey);
+    });
+    console.log(details);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <CreateArea updateNotes={addNote} />
+      {details.map((x, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            deleteNodefunc={deleteNode}
+            title={x.headingDetail}
+            content={x.contentDetail}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
